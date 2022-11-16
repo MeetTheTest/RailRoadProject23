14ccd79b1dc5632c3e4359cfe7d95fea58ad7bb6
Ruslan Kuleshov <ruslan.kuleshov@jetbrains.com>
2022-11-17 00:29:18 +0100

    fixup! Added File Reader and Writer

--- src/main/java/test/controller/StationControllerTest.java
+++ src/main/java/test/controller/StationControllerTest.java
@@ -13,5 +13,7 @@ public class StationControllerTest {
         System.out.println(stationController.getAllEntities());
         System.out.println(stationController.getAllEntities());
         System.out.println(stationController.getAllEntities());
+        System.out.println(stationController.getAllEntities());
+        System.out.println(stationController.getAllEntities());
     }
 }
